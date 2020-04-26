c6cad0515ffb8920694ee48a11288d206a838726
Daniel Hernandez <tamdannyhdz@gmail.com>
2020-04-26 00:39:19 -0500

    Include node_modules in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel, lastRun } = require("gulp");
 const path = require("path");
 
-const buildDist = () => {
-  console.log("Building dist/...");
+const distSrc = () => src("./UeberPlayer.widget/**/*", {since: lastRun(distSrc)}).pipe(dest("./dist/UeberPlayer.widget"));
 
-  const widget = src("./UeberPlayer.widget/**/*").pipe(dest("./dist/UeberPlayer.widget"));
-
-  return widget;
-}
+const distModules = () =>  src("./node_modules/**/*", {since: lastRun(distModules)}).pipe(dest("./dist/UeberPlayer.widget/node_modules"));
 
 const dev = () => {
   const widgetLocation = `${process.env.HOME}/Library/Application Support/Übersicht/widgets`;
 
   watch("./UeberPlayer.widget/**/*", { ignoreInitial: false }, function watcher() {
-    return buildDist().pipe(dest(path.join(widgetLocation, "UeberPlayer.widget")));
+    return distSrc().pipe(dest(path.join(widgetLocation, "UeberPlayer.widget")));
+  });
+
+  watch("./node_modules/**/*", { ignoreInitial: false }, function watcher() {
+    return distModules().pipe(dest(path.join(widgetLocation, "UeberPlayer.widget/node_modules")));
   });
 }
 
-exports.default = buildDist;
+exports.default = parallel(distSrc, distModules);
 exports.dev = dev;
